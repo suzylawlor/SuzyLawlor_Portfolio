94f92417cd56b15b222a00c09791150b89d5ae39
Suzy Lawlor <125146720+suzylawlor@users.noreply.github.com>
2023-11-07 20:51:24 +0000

    Add files via upload

--- src/pages/Inspiration/Quotes.js
+++ src/pages/Inspiration/Quotes.js
@@ -62,20 +62,46 @@ const QuoteGenerator = () => {
         <span style={{ "--i": 1 }}></span>
         <span style={{ "--i": 2 }}></span>
         <span style={{ "--i": 3 }}></span>
+        <span style={{ "--i": 4 }}></span>
+        <span style={{ "--i": 5 }}></span>
+        <span style={{ "--i": 6 }}></span>
+        <span style={{ "--i": 7 }}></span>
+        <span style={{ "--i": 8 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
       </div>
       <div className={styles.glowing}>
         <span style={{ "--i": 1 }}></span>
         <span style={{ "--i": 2 }}></span>
         <span style={{ "--i": 3 }}></span>
+        <span style={{ "--i": 4 }}></span>
+        <span style={{ "--i": 5 }}></span>
+        <span style={{ "--i": 6 }}></span>
+        <span style={{ "--i": 7 }}></span>
+        <span style={{ "--i": 8 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
       </div>
       <div className={styles.glowing}>
         <span style={{ "--i": 1 }}></span>
         <span style={{ "--i": 2 }}></span>
         <span style={{ "--i": 3 }}></span>
+        <span style={{ "--i": 4 }}></span>
+        <span style={{ "--i": 5 }}></span>
+        <span style={{ "--i": 6 }}></span>
+        <span style={{ "--i": 7 }}></span>
+        <span style={{ "--i": 8 }}></span>
+        <span style={{ "--i": 9 }}></span>
+        <span style={{ "--i": 10 }}></span>
+        <span style={{ "--i": 11 }}></span>
+        <span style={{ "--i": 12 }}></span>
       </div>
       <div className={styles.wrapper}>
         <div className={styles.container}>
-         
           {/* Has the data been loaded? (handleClick function activated?) */}
           {loaded ? (
             // If data has been loaded, render data from quote and author variables
